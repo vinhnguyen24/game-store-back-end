229b4ddc0655e8d2ab7ed0fdd8b82888ac2f3580
Vinh NguyenT <vinh.nguyent@s3corp.com.vn>
2025-07-23 14:50:14 +0700

    feat: update use cdn & db

--- config/database.ts
+++ config/database.ts
@@ -31,15 +31,9 @@ export default ({ env }) => {
     postgres: {
       connection: {
         connectionString: env("DATABASE_URL"),
-        host: env("DATABASE_HOST", "localhost"),
-        port: env.int("DATABASE_PORT", 5432),
-        database: env("DATABASE_NAME", "strapi"),
-        user: env("DATABASE_USERNAME", "strapi"),
-        password: env("DATABASE_PASSWORD", "strapi"),
-        ssl: env.bool("DATABASE_SSL", true)
-          ? { rejectUnauthorized: false }
-          : false,
-        schema: env("DATABASE_SCHEMA", "public"),
+        ssl: {
+          rejectUnauthorized: false,
+        },
       },
       pool: {
         min: env.int("DATABASE_POOL_MIN", 2),
